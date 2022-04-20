fc67448bfd32b55140351d89da957d681701bc06
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-04-21 00:41:07 +0300

    fixed result type of axios call

--- src/store/sagas/photosSaga.ts
+++ src/store/sagas/photosSaga.ts
@@ -1,13 +1,16 @@
 import axios from 'axios';
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, SagaReturnType, takeEvery } from 'redux-saga/effects';
 import { IPhoto } from '../../types/photos';
 import { photosSlice } from '../reducers/photosSlice';
 
 const fetchPhotosFromApi = () =>
-  axios.get('https://jsonplaceholder.typicode.com/users?_limit=24');
+  axios.get('https://jsonplaceholder.typicode.com/photos?_limit=24');
 
 function* fetchPhotosWorker() {
-  const data: IPhoto[] = yield call(fetchPhotosFromApi);
+  const response: SagaReturnType<typeof fetchPhotosFromApi> = yield call(
+    fetchPhotosFromApi
+  );
+  const data: IPhoto[] = response.data;
   yield put(photosSlice.actions.photosFetchingSuccessAsync(data));
 }
 
